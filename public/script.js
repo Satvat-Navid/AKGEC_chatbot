b25151886fd4ee615de4d58aeefad7cc1901be13
Satvat Navid <satvatnavid@gmail.com>
2025-09-14 01:50:38 +0530

    changes for deployement 1

--- public/script.js
+++ public/script.js
@@ -69,7 +69,7 @@ document.addEventListener("DOMContentLoaded", () => {
         
         try {
             // const response = await fetch("/api/chat", {
-            const response = await fetch("http://127.0.0.1:8000/chat", {
+            const response = await fetch("https://akgec-chatbot.vercel.app/api/chat/", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
