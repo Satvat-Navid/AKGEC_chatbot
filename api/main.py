0aa71274d4b464557c10c0915d4d7011511bf2fc
Satvat Navid <satvatnavid@gmail.com>
2025-09-14 16:48:56 +0530

    Changes for deployment 12

--- api/main.py
+++ api/main.py
@@ -21,7 +21,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.post("/chat")
+# @app.post("/chat")
+@app.post("/")
 def chat(request: ChatRequest):
     request_start_time = time.time()
     logger.info("--- New Chat Request Received ---")
